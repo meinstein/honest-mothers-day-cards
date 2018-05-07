ec9c8e5187dbb7866a4e4c1a3d5d8a1ec67e7f44
meinstein <max@datascience.com>
2018-05-07 01:22:06 -0700

    add fade over placeholder

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,37 +1,43 @@
 import React from 'react'
 class Card extends React.Component {
 	state = {
-		img: null
+		loaded: false
 	}
 
 	componentDidMount() {
-		var img = new Image()
-		img.src = `/static/cards/png/${this.props.link}.png`
-		img.onload = () => this.setState({ img })
+		if (this.img.complete) {
+			this.setState({ loaded: true })
+		}
 	}
 
 	render() {
 		const { title, link, style, bgColor } = this.props
-
 		return (
-			<React.Fragment>
-				{!this.state.img ? (
-					<svg
-						width="600"
-						height="777"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-						style={{ maxWidth: '100%', height: 'auto' }}
-					>
-						<rect width="100%" height="100%" y="0" x="0" fill={bgColor} />
-					</svg>
-				) : (
-					<img
-						style={{ maxWidth: '100%', height: 'auto', ...style }}
-						src={this.state.img.src}
-					/>
-				)}
-			</React.Fragment>
+			<div style={{ position: 'relative' }}>
+				<svg
+					width="600"
+					height="777"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					style={{ maxWidth: '100%', height: 'auto' }}
+				>
+					<rect width="100%" height="100%" y="0" x="0" fill={bgColor} />
+				</svg>
+				<img
+					ref={node => (this.img = node)}
+					className={this.state.loaded ? 'fade-in-card' : 'hidden-card'}
+					style={{
+						maxWidth: '100%',
+						height: 'auto',
+						position: 'absolute',
+						top: 0,
+						left: 0,
+						...style
+					}}
+					onLoad={() => this.setState({ loaded: true })}
+					src={`/static/cards/png/${this.props.link}.png`}
+				/>
+			</div>
 		)
 	}
 }
